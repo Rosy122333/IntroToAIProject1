0e7ce0869a3997b29b926f24d1c70240399e2f31
Rosy122333 <80847580+Rosy122333@users.noreply.github.com>
2023-09-11 16:44:36 -0400

    added in a way to read a file may not be correct

--- main.java
+++ main.java
@@ -1,4 +1,31 @@
-
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+import java.net.*;
 
 public class main {
+
+
+
+  public static void main(String[] args) {
+    try {
+        URL path = main.class.getResource("groupname.go");
+      File myObj = new File(path.getFile());
+      Scanner myReader = new Scanner(myObj);
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println(data);
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("File not there");
+      e.printStackTrace();
+    }
+  }
+
+
+
+
+
+
 }
