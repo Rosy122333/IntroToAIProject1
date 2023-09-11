d7aefe8e973a01e9598950c4243baac66e30751d
aapeeriz <128171148+aapeeriz@users.noreply.github.com>
2023-09-11 16:36:00 -0400

    made main class

--- Game.java
+++ Game.java
@@ -1,10 +1,11 @@
+import java.util.ArrayList;
+
 public class Game {
     int width;
     int height;
+    ArrayList<Edge> edges;
 
-    Edge[] edges;
-
-    public Game(int width, int height, Edge[] edges) {
+    public Game(int width, int height, ArrayList<Edge> edges) {
         this.width = width;
         this.height = height;
         this.edges = edges;
@@ -18,14 +19,6 @@ public class Game {
         return isOnBoard(e.getN1()) && isOnBoard(e.getN2());
     }
 
-    public boolean isOnBoard(int x, int y) {
-        return isOnBoard(new Node(x, y));
-    }
-
-    public boolean isOnBoard(int x1, int y1, int x2, int y2) {
-        return isOnBoard(new Edge(new Node(x1, y1), new Node(x2, y2)));
-    }
-
     public boolean isAlrOnBoard(Edge e) {
         for (Edge edge : edges) {
             if (edge.equals(e)) {
@@ -34,4 +27,11 @@ public class Game {
         }
         return false;
     }
+
+    public boolean isLegal(Edge e) {
+        if (isOnBoard(e) && !isAlrOnBoard(e))
+            return edges.add(e);
+        return false;
+    }
+
 }
