c3b64bc9d0c82a8c5db9efd2f2d63eb9c178601e
Matt <65925728+MatthewAdragna@users.noreply.github.com>
2023-09-18 11:40:09 -0400

    Changes to edge handling
    +Created Edge.equals() that allows for reversed node order of the same edge to return true.
    
    +Added a statement that bails out of adding a passmove edge [(0,0),(0,0)]

--- Edge.java
+++ Edge.java
@@ -23,8 +23,24 @@ public class Edge {
         this.n2 = n2;
     }
 
-    public String toString(){
+    public String toString() {
         return " " + n1 + " " + n2; // ToString in form " x1,y1 x2,y2"
+    }
+
+    // Given an Edge returns a boolean value of whether or not the inputted edge is
+    // equal to this one
+    // This is needed due to the fact that inverse order of inputted nodes are the
+    // same nodes but saved differently in the Edge Objects
+    // Could be unneeded given a way to normalize inputted nodes into a normalized
+    // storage structure
+    public boolean equals(Edge in) {
+        // given theoretically the nodes are not the same as that would be an illegal
+        // move except for a pass
+        return (n1.equals(in.n2) || n1.equals(in.n1)) && (n2.equals(in.n2) || n2.equals(in.n1))
+        // && !(n1.equals(n2)) // This is theoretically not needed due to this -> should
+        // also be the first term of the boolean statement if so to increase performance
+        ;
+
     }
 
 }
--- Game.java
+++ Game.java
@@ -62,7 +62,8 @@ public class Game {
         int n1X = Integer.parseInt(moveData[1]);
         int n1Y = Integer.parseInt(moveData[2]);
         int n2X = Integer.parseInt(moveData[3]);
-        int n2Y = Integer.parseInt(moveData[4]);
+        int n2Y = Integer.parseInt(moveData[4])
+        if (n1X == 0 && n1Y == 0 & n2X == 0 && n2Y == 0) return; // In case of pass move -> do nothing
         edges.add(new Edge(new Node(n1X, n1Y), new Node(n2X, n2Y)));
     }
 
