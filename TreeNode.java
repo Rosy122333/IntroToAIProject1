081bf3625c05e2de2b1492d8ca1c80e09eea1d24
aapeeriz <128171148+aapeeriz@users.noreply.github.com>
2023-09-19 23:15:56 -0400

    board methods

--- TreeNode.java
+++ TreeNode.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class TreeNode {
+import java.util.ArrayList;
+
+public class TreeNode {
+    private Board board;
+    private TreeNode parent;
+    private Edge move;
+    private int utility;
+    private boolean player;
+    static int MIN = -10000000;
+
+    public TreeNode(Board board, TreeNode parent, boolean player, Edge move) {
+        this.board = board;
+        this.parent = parent;
+        this.player = player;
+        this.move = move;
+        this.utility = MIN;
+         }
+
+    public Board getBoard() {
+        return board;
+    }
+    public TreeNode getParent() {
+        return parent;
+    }
+    public boolean getPlayer() {
+        return player;
+    }
+
+    public int getUtility() {
+        return utility;
+    }
+
+    public void setUtility(int utility) {
+        this.utility = utility ;
+    }
+
+    public Edge getEdge() {
+        return this.move ;
+    }
+
+    public void setEdge(Edge edge) {
+        this.move = edge ;
+    }
 }
