11909edbc3c04e81d636db25d6897bb81a07fd96
Rosy122333 <80847580+Rosy122333@users.noreply.github.com>
2023-09-25 18:56:12 -0400

    I made something not working though

--- alphaBetaSolver.java
+++ alphaBetaSolver.java
@@ -9,46 +9,183 @@ public class alphaBetaSolver extends Solver {
     long boundedTime = 9 * 1070;
     short depthLevelMax = 2;
 
+    private static final int TIME_LIMIT_MILLIS = 9000;
+	private static final int EVALS_PER_SECOND = 100;
+	private static final int emptyPosition = 0;
+	private static final int aiPosition = 1;
+	private static final int humanPosition = 2;
+	private static final int winCutoff = 41; //idk what this was 
+    boolean searchCutoff;
+
+
+
+
+
+
+
+
+    // @Override
+    // public Edge getBestMove(Game in) {
+    //     timeStarted = System.currentTimeMillis();
+    //     //while (time < 10 seconds) {
+          
+
+    //     }
+    //     //return 
+    
+
+    // public Bound depthFirstSearch(Board in, int alpha, int beta, short depthLeft) {
+        
+    //         return new Bound(null, heuristicFunction(in));
+    //     }
+            
+    
+
+
+
+
+    //
+	// chooseMove() scores each of the possible moves that can be made from the given AIGameState, and returns the move with the highest eval score
+	//
     @Override
     public Edge getBestMove(Game in) {
-        timeStarted = System.currentTimeMillis();
-        return this.depthFirstSearch(in.currentBoard, MIN, MAX, depthLevelMax).edge;
-    }
+		long startTime = System.currentTimeMillis();
+        Board boardConfiguration = in.currentBoard;
+		//boolean maximizing = in.currentBoard.isMyTurn;
+		int maxScore = Integer.MIN_VALUE;
+		Edge bestMove = null;
+		
+		ArrayList<Edge> moves = boardConfiguration.allPossibleMoves;
+        int length = moves.size();
+        //Collections.shuffle(moves);
+		
+		for (Edge move : moves) {
+            //System.out.println("HRERHERERER");
+			//
+			// Copy the current game state
+			//
+			Board newState = boardConfiguration.clone();
+			
+			newState.addMove(move);
+			
+			//
+			// Compute how long to spend looking at each move
+			//
+			long searchTimeLimit = ((TIME_LIMIT_MILLIS - 1000) / (moves.size()));
+			
+			int score = iterativeDeepeningSearch(newState, searchTimeLimit);
+            System.out.println("score"+ score);
+			
+			//
+			// If the search finds a winning move
+            /*if (searchResult >= winCutoff) {
+				return searchResult;
+			} */
+			//
+            length = length - 1;
+			if ((System.currentTimeMillis()- startTime > 9000)||(length==0)) { //break out condition
+				return move; //not getting here
+			}
+			
+			if (score > maxScore) { //what is score and maxScore
+				maxScore = score;
+				bestMove = move;
+                System.out.println("score"+ score);
+			}
+		}
+        System.out.println("maxscore"+maxScore);
+		return bestMove;
+	}
+	
+	//
+	// Run an iterative deepening search on a game state, taking no longrer than the given time limit
+	//
+	private int iterativeDeepeningSearch(Board boardConfiguration, long timeLimit) {
+		long startTime = System.currentTimeMillis();
+		long endTime = startTime + timeLimit;
+		int depth = 3; //1 
+		int score = 0;
+		searchCutoff = false;
+		
+		while (true) {
+			long currentTime = System.currentTimeMillis();
+			
+			if (currentTime >= endTime) {
+				break;
+			}
+			
+			int searchResult = search(boardConfiguration, depth, Integer.MIN_VALUE, Integer.MAX_VALUE, currentTime, endTime - currentTime);
+			
+			//
+			// If the search finds a winning move, stop searching
+			//searchResult >= winCutoff
+			if (System.currentTimeMillis()- startTime > 9000) {
+				return searchResult;
+			}
+			
+            //!searchCutoff
+			if (!searchCutoff) {
+				score = boardConfiguration.relativeScore;
+			}
+			
+			depth++;
+		}
+		
+		return score;
+	}
+	
+	//
+	// search() will perform minimax search with alpha-beta pruning on a game state, and will cut off if the given time
+	// limit has elapsed since the beginning of the search
+	//
+	private int search(Board boardConfiguration, int depth, int alpha, int beta, long startTime, long  timeLimit) {
+		ArrayList<Edge> moves = boardConfiguration.allPossibleMoves;
+        //ArrayList<AIGameMove> moves = state.validMoves();
+		boolean maximizing = boardConfiguration.isMyTurn;
+		int savedScore = (maximizing) ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+		///int score = Evaluator.eval(boardConfiguration);
+        int score = boardConfiguration.relativeScore;
+		long currentTime = System.currentTimeMillis();
+		long elapsedTime = (currentTime - startTime);
+		
+		if (elapsedTime >= timeLimit) {
+			searchCutoff = true;
+		}
+		
+		//
+		// If this is a terminal node or a win for either player, abort the search
+		//
+		if (searchCutoff || (depth == 0) || (moves.size() == 0)) {
+            System.out.println("relative score "+score);
+			return score;
+		}
+		
+		if (boardConfiguration.isMyTurn) {
+			for (Edge move : moves) {
+				Board childState = boardConfiguration.clone();
+				childState.addMove(move);
 
-    public Bound depthFirstSearch(Board in, int alpha, int beta, short depthLeft) {
-        int movesLeft = in.allPossibleMoves.size();
-        if (depthLeft <= 0 || (System.currentTimeMillis() - timeStarted >= boundedTime) || (movesLeft == 0))
-            return new Bound(null, heuristicFunction(in));
-
-        Collections.shuffle(in.allPossibleMoves);
-
-        Bound boundOut = new Bound(null, in.isMyTurn ? MIN : MAX);
-
-        for (int i = 0; i < movesLeft; i++) {
-            Board workingBoard = in.clone();
-            workingBoard.addMove(in.allPossibleMoves.get(i));
-
-            boolean flag = false;
-            if (workingBoard.relativeScore == in.relativeScore) {
-                workingBoard.isMyTurn = !workingBoard.isMyTurn;
-                flag = true;
-            }
-            Bound workingBound = depthFirstSearch(workingBoard, alpha, beta, --depthLeft);
-
-            int workingUtility = workingBound.utility;
-
-            if (in.isMyTurn ? boundOut.utility < workingUtility : boundOut.utility > workingUtility) {
-                boundOut.utility = workingUtility;
-                boundOut.edge = in.allPossibleMoves.get(i);
-            }
-
-            if (flag)
-                if (in.isMyTurn ? workingUtility >= beta : workingUtility <= alpha)
-                    return boundOut;
-
-        }
-
-        return new Bound(null, heuristicFunction(in));
-    }
-
+				alpha = Math.max(alpha, search(childState, depth - 1, alpha, beta, startTime, timeLimit));
+				
+				if (beta <= alpha) {
+					break;
+				}
+			}
+			
+			return alpha;
+		} else {
+			for (Edge move : moves) {
+				Board childState = boardConfiguration.clone();
+				childState.addMove(move);
+				
+				beta = Math.min(beta, search(childState, depth - 1, alpha, beta, startTime, timeLimit));
+					
+				if (beta <= alpha) {
+					break;
+				}
+			}
+			
+			return beta;
+		}
+	}
 }
