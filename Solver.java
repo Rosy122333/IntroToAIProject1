00a4a238e078751c76adb4c1e02dc4593f3f158d
aapeeriz <128171148+aapeeriz@users.noreply.github.com>
2023-09-25 07:29:10 -0400

    recording who is doing what move for heuristic fxn (only changing board class)

--- Solver.java
+++ Solver.java
@@ -2,6 +2,13 @@ public abstract class Solver {
 
     protected int heuristicFunction(Board in){
         return in.relativeScore;
+        // int score = 0;
+        // if (in.countOpenEdges() % 2 == 0) {
+        //     score++;
+        // } else {
+        //     score--;
+        // }
+        // return score;
     }
 
     public abstract Edge getBestMove(Game in);
